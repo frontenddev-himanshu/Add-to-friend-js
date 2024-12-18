e74cff80ae9875792f7a412426801b04dd085b40
frontenddev-himanshu <rajh5343@gmail.com>
2024-12-18 20:55:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// define
+let head = document.getElementById("heading");
+let btn = document.getElementById("btn");
+
+
+// event
+btn.addEventListener("click", function(){
+
+// heading change
+head.innerText = "Added To Friend";
+
+// button text change
+btn.innerText = "Added";
+
+})
